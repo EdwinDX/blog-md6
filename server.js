e6b5702aa435f0f5a5859e02f86591bf2de4ef8c
EdwinDX <you@example.comletiendung502@gmail.com>
2022-10-20 16:36:16 +0700

    ok

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
